3fb5f5cbf7f0e9ed87ea120da7e2583346223eda
MD HASAN MONSUR <hasanmcse@gmail.com>
2024-10-21 00:36:07 +0600

    hasan

---
+++ PaginationWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PaginationWebApi.DTOs;
+using PaginationWebApi.Models;
+using PaginationWebApi.Services;
+
+namespace PaginationWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductRepository _productRepository;
+
+        public ProductsController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<PaginatedResponse<Record>>> GetProducts(int pageNumber = 1, int pageSize = 10)
+        {
+            var products = await _productRepository.GetPaginatedProducts(pageNumber, pageSize);
+            var totalCount = await _productRepository.GetTotalProductsCount();
+
+            var response = new PaginatedResponse<Record>
+            {
+                Items = products,
+                TotalCount = totalCount,
+                PageSize = pageSize,
+                PageNumber = pageNumber
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ PaginationWebApi/Services/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using PaginationWebApi.Data;
+using PaginationWebApi.Models;
+using System.Data;
+
+namespace PaginationWebApi.Services
+{
+    public class ProductRepository
+    {
+        private readonly DapperContext _connectionString;
+
+        public ProductRepository(DapperContext connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task<IEnumerable<Record>> GetPaginatedProducts(int pageNumber, int pageSize)
+        {
+            using (var db = _connectionString.CreateConnection())
+            {
+                var sql = "SELECT * FROM Records ORDER BY Id OFFSET @Offset ROWS FETCH NEXT @PageSize ROWS ONLY;";
+                return await db.QueryAsync<Record>(sql, new { Offset = (pageNumber - 1) * pageSize, PageSize = pageSize });
+            }
+        }
+
+        public async Task<int> GetTotalProductsCount()
+        {
+            using (var db = _connectionString.CreateConnection())
+            {
+                var sql = "SELECT COUNT(*) FROM Records;";
+                return await db.ExecuteScalarAsync<int>(sql);
+            }
+        }
+    }
+}
